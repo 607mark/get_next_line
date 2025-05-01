6d12acad7c600dee136dac928b43c29b043e40df
607mark <40577872+607mark@users.noreply.github.com>
2025-05-01 13:46:42 +0300

    main.c

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "get_next_line.h"
+
+int main(int argc, char **argv)
+{
+    int fd;
+    char *line;
+
+    if (argc == 2)
+    {
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1)
+        {
+            perror("Error opening file");
+            return (1);
+        }
+    }
+    else
+    {
+        fd = 0;
+    }
+
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
+
+    if (fd != 0)
+        close(fd);
+
+    return (0);
+}
